a515a7790c9c700b43ad9b53a0c2a37ad9020939
onekki <285079772@qq.com>
2019-06-28 21:58:13 +0800

    简要修改config

--- config.py
+++ config.py
@@ -10,12 +10,10 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # 数据库URI
 MYSQL_DATABASE_URI = 'mysql+pymysql://root:123456@39.107.230.35:3306/onekki_site'
 
-# 查询跟踪，不太需要，False，不占用额外的内存
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = MYSQL_DATABASE_URI
-    
+    # 查询跟踪，不太需要，False，不占用额外的内存
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     @staticmethod
     def init_app(app):
         pass
